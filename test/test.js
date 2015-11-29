fc52df066d1ff603d73edfd570070b589697e79f
Sérgio Ramos <mail@sergioramos.me>
2015-11-29 03:53:51 +0000

    toElement

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var lab = require('lab').script();
 var assert = require('assert');
-var React = require('../');
+var toElement = require('../');
 
 lab.suite('test', function() {
   lab.test('test', function(done) {
